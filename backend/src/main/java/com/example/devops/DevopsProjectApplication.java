53cde11c0fbb15cb4a699c0470cb10ccd9ab96d7
HariniSathish10 <harinikalaesh@gmail.com>
2026-06-02 15:12:13 +0530

    Add files via upload

---
+++ backend/src/main/java/com/example/devops/DevopsProjectApplication.java
@@ -0,0 +1,11 @@
+package com.example.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsProjectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DevopsProjectApplication.class, args);
+    }
+}
